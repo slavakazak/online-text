4ec8df856060981a8b59e432568c920596df052e
slavakazak <slavakazak3@gmail.com>
2024-03-19 21:09:42 +0300

    firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { getDatabase, ref, set, get, onValue } from "firebase/database"
 
 export default function App() {
 	const [text, setText] = useState('')
 
 	function changeHandler(e) {
 		setText(e.target.value)
-
-		console.log(e.target.value)
+		const db = getDatabase()
+		set(ref(db, 'text'), e.target.value)
 	}
+
+	useEffect(() => {
+		const db = getDatabase()
+		onValue(ref(db, 'text'), snapshot => {
+			const data = snapshot.val()
+			setText(data)
+		})
+	}, [])
 
 	return (
 		<div className="App">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyD5OPU-mjP-jWa-nq5AvYI87OL9gZcXcoo",
+	authDomain: "online-text-b570e.firebaseapp.com",
+	databaseURL: "https://online-text-b570e-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "online-text-b570e",
+	storageBucket: "online-text-b570e.appspot.com",
+	messagingSenderId: "136715642771",
+	appId: "1:136715642771:web:5267177ad588dd2fbd6a66"
+}
+initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
