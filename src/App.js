1c960e4542fe00b0cbd0f2c246b691fb4c549dff
slavakazak <slavakazak3@gmail.com>
2024-03-19 14:53:00 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [text, setText] = useState('')
+
+	function changeHandler(e) {
+		setText(e.target.value)
+
+		console.log(e.target.value)
+	}
+
+	return (
+		<div className="App">
+			<textarea placeholder="Enter text" value={text} onChange={changeHandler} />
+		</div>
+	)
 }
-
-export default App;
