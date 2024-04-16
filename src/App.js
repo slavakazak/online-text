39e403ceb930151f2fe653d80471ae2e9951bc75
slavakazak <slavakazak3@gmail.com>
2024-04-16 16:36:14 +0300

    tabs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import { useState, useEffect } from "react"
-import { getDatabase, ref, set, get, onValue } from "firebase/database"
+import { getDatabase, ref, set, get, child, onValue } from "firebase/database"
 
 export default function App() {
-	const [text, setText] = useState('')
+  const [text, setText] = useState('')
+  const [tabs, setTabs] = useState(['All', 'Cartel', 'Attic'])
+  const [current, setCurrent] = useState('All')
 
-	function changeHandler(e) {
-		setText(e.target.value)
-		const db = getDatabase()
-		set(ref(db, 'text'), e.target.value)
-	}
+  function changeHandler(e) {
+    setText(e.target.value)
+    const db = getDatabase()
+    set(ref(db, current), e.target.value)
+  }
 
-	useEffect(() => {
-		const db = getDatabase()
-		onValue(ref(db, 'text'), snapshot => {
-			const data = snapshot.val()
-			setText(data)
-		})
-	}, [])
+  function tabClickHandler(tab) {
+    return () => {
+      setCurrent(tab)
+      const dbRef = ref(getDatabase())
+      get(child(dbRef, tab)).then((snapshot) => {
+        if (snapshot.exists()) {
+          setText(snapshot.val())
+        } else {
+          setText('')
+        }
+      }).catch((error) => {
+        setText('')
+        console.error(error)
+      })
+    }
+  }
 
-	return (
-		<div className="App">
-			<textarea placeholder="Enter text" value={text} onChange={changeHandler} />
-		</div>
-	)
+  useEffect(() => {
+    const db = getDatabase()
+    onValue(ref(db, current), snapshot => {
+      const data = snapshot.val()
+      setText(data)
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="tabs">
+        {tabs.map((tab, i) => (
+          <div
+            key={i}
+            className={'tab' + (tab == current ? ' active' : '')}
+            onClick={tabClickHandler(tab)}>
+            {tab}
+          </div>
+        ))}
+      </div>
+      <textarea placeholder="Enter text" value={text} onChange={changeHandler} />
+    </div>
+  )
 }
