c89422377cb0bd4f397c78df133f6561a0aaff55
slavakazak <slavakazak3@gmail.com>
2024-04-18 15:58:18 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,49 +2,28 @@ import { useState, useEffect } from "react"
 import { getDatabase, ref, set, get, child, onValue } from "firebase/database"
 
 export default function App() {
-  const [text, setText] = useState('')
-  const [tabs, setTabs] = useState(['All', 'Cartel', 'Attic'])
+  const [tabs, setTabs] = useState({ 'All': '', 'Attic': '', 'Cartel': '' })
   const [current, setCurrent] = useState('All')
 
-  function changeHandler(e) {
-    setText(e.target.value)
-    const db = getDatabase()
-    set(ref(db, current), e.target.value)
-  }
-
-  function tabClickHandler(tab) {
-    return () => {
-      setCurrent(tab)
-      const dbRef = ref(getDatabase())
-      get(child(dbRef, tab)).then(snapshot => {
-        if (snapshot.exists()) {
-          setText(snapshot.val())
-        } else {
-          setText('')
-        }
-      }).catch((error) => {
-        setText('')
-        console.error(error)
-      })
+  function changeHandler(tab) {
+    return e => {
+      setTabs(prev => ({ ...prev, [tab]: e.target.value }))
+      set(ref(getDatabase(), tab), e.target.value)
     }
   }
 
-  useEffect(() => {
-    const db = getDatabase()
-    tabs.forEach(tab => {
-      onValue(ref(db, tab), snapshot => {
-        if (snapshot.key == current) {
-          setText(snapshot.val())
-        }
-      })
-    })
+  function tabClickHandler(tab) {
+    return () => setCurrent(tab)
+  }
 
-  }, [tabs, current])
+  useEffect(() => {
+    onValue(ref(getDatabase(), '/'), snapshot => setTabs(snapshot.val()))
+  }, [])
 
   return (
     <div className="App">
       <div className="tabs">
-        {tabs.map((tab, i) => (
+        {Object.keys(tabs).map((tab, i) => (
           <div
             key={i}
             className={'tab' + (tab == current ? ' active' : '')}
@@ -53,7 +32,17 @@ export default function App() {
           </div>
         ))}
       </div>
-      <textarea placeholder="Enter text" value={text} onChange={changeHandler} />
+      <div className="pages">
+        {Object.keys(tabs).map((tab, i) => (
+          <textarea
+            key={i}
+            placeholder="Enter text"
+            className={tab == current ? 'active' : ''}
+            value={tabs[tab]}
+            onChange={changeHandler(tab)}
+          />
+        ))}
+      </div>
     </div>
   )
 }
